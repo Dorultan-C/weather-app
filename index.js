f149e04e04e513d26d1c49ba0d86aef90a3b497c
Cristian Dorultan <c.dorultan@gmail.com>
2024-08-30 11:34:09 +0100

    30.45 change pc

---
+++ index.js
@@ -0,0 +1,20 @@
+const apiKey = 'e244328dfe7e0075a76492fe1dad86af';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metricq=';
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json();
+
+document.querySelector('.city').innerHTML = data.name;
+document.querySelector('.temp').innerHTML = Math.round(data.main.temp)  + '°C';
+document.querySelector('.humidity').innerHTML = data.main.humidity +' %';
+document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h';
+}
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
+})
+
+
