fe9cead63eb68bbb945d79eb6c94965e27a2742e
Cristian Dorultan <c.dorultan@gmail.com>
2024-09-02 14:45:40 +0100

    errors handle

--- index.js
+++ index.js
@@ -9,32 +9,40 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
     
-    
-
+    if(response.status == 404) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    }else{
  document.querySelector('.city').innerHTML = data.name;
  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
  document.querySelector('.humidity').innerHTML = data.main.humidity +' %';
  document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h';
 
- if(data.weather[0].main == 'Clouds') {
+
+     if(data.weather[0].main == 'Clouds') {
     weatherIcon.src = 'images/clouds.png'
- }
- else if(data.weather[0].main == 'Clear') {
+     }
+     else if(data.weather[0].main == 'Clear') {
     weatherIcon.src = 'images/clear.png'
- }
- else if(data.weather[0].main == 'Rain') {
+     }
+     else if(data.weather[0].main == 'Rain') {
     weatherIcon.src = 'images/rain.png'
- }
- else if(data.weather[0].main == 'Drizzle') {
+    }
+     else if(data.weather[0].main == 'Drizzle') {
     weatherIcon.src = 'images/drizzle.png'
- }
- else if(data.weather[0].main == 'Mist') {
+    }
+     else if(data.weather[0].main == 'Mist') {
     weatherIcon.src = 'images/mist.png'
- }
+    }
 
  document.querySelector('.weather').style.display = 'block';
+ document.querySelector('.error').style.display = 'none';
 
 }
+
+    }
+
+
 
  searchBtn.addEventListener('click', ()=>{
      checkWeather(searchBox.value);
