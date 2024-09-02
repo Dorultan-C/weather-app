f9706296ebc9ffedd2a1c3759c023bbef1c7f327
Cristian Dorultan <c.dorultan@gmail.com>
2024-09-02 14:10:50 +0100

    clean cod,

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const apiKey = 'e244328dfe7e0075a76492fe1dad86af';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metricq=';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
+    var data = await response.json();
+    
+    
 
-document.querySelector('.city').innerHTML = data.name;
-document.querySelector('.temp').innerHTML = Math.round(data.main.temp)  + '°C';
-document.querySelector('.humidity').innerHTML = data.main.humidity +' %';
-document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h';
+ document.querySelector('.city').innerHTML = data.name;
+ document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+ document.querySelector('.humidity').innerHTML = data.main.humidity +' %';
+ document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h';
 }
 
-searchBtn.addEventListener('click', ()=>{
-    checkWeather(searchBox.value);
-})
+ searchBtn.addEventListener('click', ()=>{
+     checkWeather(searchBox.value);
+ })
+
+
 
 
